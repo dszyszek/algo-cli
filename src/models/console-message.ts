1ac857a27b64b43ea8e7a9f9c552a71d7a16f475
Damian Szyszka <dszys@softserveinc.com>
2021-04-18 23:15:24 +0200

    Add model for console messages

---
+++ src/models/console-message.ts
@@ -0,0 +1,8 @@
+export enum ConsoleMessage {
+  TITLE = 'ALGO-CLI',
+  BANNER = 'Create/run/compare algorithms with ease',
+  CREATE = 'CREATE',
+  RUN = 'RUN',
+  COMPARE_PERFORMANCE = 'COMPARE PERFORMANCE',
+  GENERATE = 'GENERATE',
+}
