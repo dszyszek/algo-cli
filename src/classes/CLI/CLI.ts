1f5582fe756c8fc1cda9e610db7a7386290a63bc
Damian Szyszka <dszys@softserveinc.com>
2021-04-20 21:24:54 +0200

    Handle situation where file already exist

--- src/classes/CLI/CLI.ts
+++ src/classes/CLI/CLI.ts
@@ -1,36 +1,61 @@
 import { CLIAbstract } from './CLIAbstract';
-import { logSuccess, logError } from '../../utils/logger';
+import { logSuccess, logError, logInfo } from '../../utils/logger';
 import { mainOptionsQuestion } from '../../questions/main-options';
 import { AlgorithmPossibleActions } from '../../models/algorithm-actions';
-import { fileNameQuestion, filePathQuestion } from '../../questions/utility';
+import {
+  fileNameQuestion,
+  filePathQuestion,
+  fileOverrideQuestion,
+} from '../../questions/utility';
 import { checkIfExist, createFile } from '../../utils/fs';
 import newAlgorithmFileTemplate from '../../templates/new-algorithm-template';
 
 export class CLI extends CLIAbstract {
+  private overrideFileHandler = async (
+    filePath: string,
+    algorithmTemplate?: string,
+  ): Promise<void> => {
+    const { file_override } = await fileOverrideQuestion();
+    if (file_override) {
+      this.createFileHandler(filePath, algorithmTemplate);
+      return;
+    }
+
+    logInfo('File was not generated');
+  };
+
+  private createFileHandler = async (
+    filePath: string,
+    algorithmTemplate?: string,
+  ): Promise<void> => {
+    createFile(filePath, algorithmTemplate);
+    logSuccess('File generated!');
+  };
+
+  private async handleCreateAlgorithm(): Promise<void> {
+    const { file_name } = await fileNameQuestion(
+      'Input name of new algorithm file',
+    );
+    const { file_path } = await filePathQuestion(
+      'Input path to new algorithm file',
+    );
+    const newFilePath = `${file_path}/${file_name}.ts`;
+
+    const fileExist = checkIfExist(newFilePath);
+
+    if (!fileExist) {
+      this.createFileHandler(newFilePath, newAlgorithmFileTemplate);
+    } else {
+      this.overrideFileHandler(newFilePath, newAlgorithmFileTemplate);
+    }
+  }
+
   protected handleMainQuestions = async () => {
     const { main_options } = await mainOptionsQuestion();
 
     switch (main_options) {
       case AlgorithmPossibleActions.CREATE_ALGORITHM:
-        const { file_name } = await fileNameQuestion(
-          'Input name of new algorithm file',
-        );
-        const { file_path } = await filePathQuestion(
-          'Input path to new algorithm file',
-        );
-        const newFilePath = `${file_path}/${file_name}`;
-
-        const fileExist = checkIfExist(newFilePath);
-
-        if (!fileExist) {
-          const newFilePath = `${file_path}/${file_name}.ts`;
-          createFile(newFilePath, newAlgorithmFileTemplate);
-          logSuccess('File generated!');
-        } else {
-          // TODO: handle question if user want to override file
-          logError('File already exists - aborting!');
-        }
-
+        this.handleCreateAlgorithm();
         break;
     }
   };
