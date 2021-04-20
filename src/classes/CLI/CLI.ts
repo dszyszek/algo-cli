1c710ecaa1ab3b4bc49900d20f885c654bc1e8f0
Damian Szyszka <dszys@softserveinc.com>
2021-04-20 22:05:56 +0200

    Move file-handling logic to File class

--- src/classes/CLI/CLI.ts
+++ src/classes/CLI/CLI.ts
@@ -1,53 +1,19 @@
 import { CLIAbstract } from './CLIAbstract';
-import { logSuccess, logError, logInfo } from '../../utils/logger';
 import { mainOptionsQuestion } from '../../questions/main-options';
 import { AlgorithmPossibleActions } from '../../models/algorithm-actions';
-import {
-  fileNameQuestion,
-  filePathQuestion,
-  fileOverrideQuestion,
-} from '../../questions/utility';
-import { checkIfExist, createFile } from '../../utils/fs';
-import newAlgorithmFileTemplate from '../../templates/new-algorithm-template';
+import { File } from '../File/File';
+import { fileNameQuestion, filePathQuestion } from '../../questions/utility';
 
 export class CLI extends CLIAbstract {
-  private overrideFileHandler = async (
-    filePath: string,
-    algorithmTemplate?: string,
-  ): Promise<void> => {
-    const { file_override } = await fileOverrideQuestion();
-    if (file_override) {
-      this.createFileHandler(filePath, algorithmTemplate);
-      return;
-    }
-
-    logInfo('File was not generated');
-  };
-
-  private createFileHandler = async (
-    filePath: string,
-    algorithmTemplate?: string,
-  ): Promise<void> => {
-    createFile(filePath, algorithmTemplate);
-    logSuccess('File generated!');
-  };
-
   private async handleCreateAlgorithm(): Promise<void> {
     const { file_name } = await fileNameQuestion(
-      'Input name of new algorithm file',
+      'Input name of the new algorithm file',
     );
     const { file_path } = await filePathQuestion(
-      'Input path to new algorithm file',
+      'Input path to the new algorithm file',
     );
-    const newFilePath = `${file_path}/${file_name}.ts`;
 
-    const fileExist = checkIfExist(newFilePath);
-
-    if (!fileExist) {
-      this.createFileHandler(newFilePath, newAlgorithmFileTemplate);
-    } else {
-      this.overrideFileHandler(newFilePath, newAlgorithmFileTemplate);
-    }
+    new File(file_name, file_path);
   }
 
   protected handleMainQuestions = async () => {
