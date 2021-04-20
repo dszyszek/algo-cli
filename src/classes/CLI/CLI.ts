40e46c3a8904e22b553fa15b921c8bb3b5c5711c
Damian Szyszka <dszys@softserveinc.com>
2021-04-20 22:53:41 +0200

    Move handleMainQuestions logic to CLIService

--- src/classes/CLI/CLI.ts
+++ src/classes/CLI/CLI.ts
@@ -1,11 +1,9 @@
 import { CLIAbstract } from './CLIAbstract';
-import { mainOptionsQuestion } from '../../questions/main-options';
-import { AlgorithmPossibleActions } from '../../models/algorithm-actions';
 import { File } from '../File/File';
 import { fileNameQuestion, filePathQuestion } from '../../questions/utility';
 
 export class CLI extends CLIAbstract {
-  private async handleCreateAlgorithm(): Promise<void> {
+  protected async handleCreateAlgorithm(): Promise<void> {
     const { file_name } = await fileNameQuestion(
       'Input name of the new algorithm file',
     );
@@ -16,13 +14,7 @@ export class CLI extends CLIAbstract {
     new File(file_name, file_path);
   }
 
-  protected handleMainQuestions = async () => {
-    const { main_options } = await mainOptionsQuestion();
-
-    switch (main_options) {
-      case AlgorithmPossibleActions.CREATE_ALGORITHM:
-        this.handleCreateAlgorithm();
-        break;
-    }
-  };
+  protected handleRunAlgorithm(): void {
+    console.log('Handle run algorithm');
+  }
 }
--- src/services/CLIService/CLIService.ts
+++ src/services/CLIService/CLIService.ts
@@ -1,7 +1,22 @@
 import { CLI } from '../../classes/CLI';
 import { displayBanner } from '../../utils/logger';
+import { mainOptionsQuestion } from '../../questions/main-options';
+import { AlgorithmPossibleActions } from '../../models/algorithm-actions';
 
 export default class CLIService extends CLI {
+  private async handleMainQuestions(): Promise<void> {
+    const { main_options } = await mainOptionsQuestion();
+
+    switch (main_options) {
+      case AlgorithmPossibleActions.CREATE_ALGORITHM:
+        this.handleCreateAlgorithm();
+        break;
+      case AlgorithmPossibleActions.RUN_ALGORITHM:
+        this.handleRunAlgorithm();
+        break;
+    }
+  }
+
   public start = (): void => {
     displayBanner();
     this.handleMainQuestions();
