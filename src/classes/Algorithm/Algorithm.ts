bd9749dadfdd78ea77e3684de5bfd1a9ca2ed49e
Damian Szyszka <dszys@softserveinc.com>
2021-04-23 00:22:29 +0200

    Implement Algorithm + AlgorithmAbstract

--- src/classes/Algorithm/Algorithm.ts
+++ src/classes/Algorithm/Algorithm.ts
@@ -1,3 +1,56 @@
-import { IAlgorithmAbstract, AlgorithmAbstract } from './AlgorithmAbstract';
+import {
+  AlgorithmAbstract,
+  PassedAlgorithm,
+  PassedAlgorithmExecutionResult,
+  PassedAlgorithmArgs,
+  AlgorithmState,
+  AlgorithmResult,
+} from './AlgorithmAbstract';
 
-export class Algorithm extends AlgorithmAbstract {}
+export class Algorithm extends AlgorithmAbstract {
+  private initialState: AlgorithmState = {
+    time: 0,
+  };
+  private state: AlgorithmState;
+  private passedAlgorithm: PassedAlgorithm;
+
+  constructor(passedAlgorithm: PassedAlgorithm, state?: AlgorithmState) {
+    super();
+    if (!passedAlgorithm) {
+      throw Error('You have to pass Algorithm!');
+    }
+
+    this.passedAlgorithm = passedAlgorithm;
+    this.state = state ?? this.initialState;
+  }
+
+  private get timeStamp(): number {
+    // return current timestamp in miliseconds (from linux epoch)
+    return new Date().getTime();
+  }
+
+  private get getState(): AlgorithmState {
+    return this.state;
+  }
+
+  private setState(state: AlgorithmState): void {
+    this.state = state;
+  }
+
+  public run(args: PassedAlgorithmArgs): AlgorithmResult {
+    const runStart = this.timeStamp;
+    const result: PassedAlgorithmExecutionResult = this.passedAlgorithm(args);
+    const runEnd = this.timeStamp;
+    const executionTime = runEnd - runStart;
+
+    const newState = {
+      time: executionTime,
+    };
+    this.setState(newState);
+
+    return {
+      result,
+      state: newState,
+    };
+  }
+}
--- src/classes/Algorithm/AlgorithmAbstract.ts
+++ src/classes/Algorithm/AlgorithmAbstract.ts
@@ -1,9 +1,21 @@
+export type PassedAlgorithm = Function;
+export type PassedAlgorithmExecutionResult = unknown;
+export type PassedAlgorithmArgs = unknown[] | unknown;
+
+export interface AlgorithmState {
+  time: number;
+}
+export interface AlgorithmResult {
+  result: PassedAlgorithmExecutionResult;
+  state: AlgorithmState;
+}
+
 export interface IAlgorithmAbstract {
-  create(): void;
+  run: (args: PassedAlgorithmArgs) => AlgorithmResult;
 }
 
 export abstract class AlgorithmAbstract implements IAlgorithmAbstract {
-  public create(): void {
+  public run(args: PassedAlgorithmArgs): AlgorithmResult {
     throw Error('method not implemented');
   }
 }
