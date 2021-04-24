a21b7ac31414aafa2f1db92d76a7742f790574ed
Damian Szyszka <dszys@softserveinc.com>
2021-04-25 00:40:08 +0200

    Write handleRunAlgorithm main part of logic

--- src/services/CLIService/CLIService.ts
+++ src/services/CLIService/CLIService.ts
@@ -3,7 +3,11 @@ import { mainOptionsQuestion } from '../../questions/main-options';
 import { AlgorithmPossibleActions } from '../../models/algorithm-actions';
 import { CompareAlgorithmsMain } from '../../models/algorithm-compare';
 import { UtilityActionMain } from '../../models/utility-actions';
+import { AlgorithmPayloadAvailableOptions } from '../../models/algorithm-payload-options';
 import { File } from '../../classes/File/File';
+import { algorithmPayloadQuestion } from '../../questions/algorithm-payload-options';
+import { AlgorithmService } from '../AlgorithmService/AlgorithmService';
+import { parseToIntArray } from '../../utils/number';
 
 export default class CLIService extends CLI {
   private async handleMainQuestions(): Promise<void> {
@@ -32,16 +36,37 @@ export default class CLIService extends CLI {
       'Input path to the new algorithm file',
     );
 
-    new File(file_name, file_path);
+    new File(file_path, file_name).create();
   }
 
   private async handleRunAlgorithm(): Promise<void> {
+    let algorithmPayload: number[];
     // get algo file name
     const file_path = await CLI.filePathQuestion(
       'Input path to the algorithm file',
     );
     // ask for payload (file or input array)
+    const { algorithm_payload_options } = await algorithmPayloadQuestion();
+
+    switch (algorithm_payload_options) {
+      case AlgorithmPayloadAvailableOptions.FROM_FILE:
+        const path_to_file = await CLI.filePathQuestion('Input path to file');
+        const fileClass = new File(path_to_file);
+        const rawFileContent = await fileClass.getContent();
+        algorithmPayload = parseToIntArray(rawFileContent);
+        break;
+      case AlgorithmPayloadAvailableOptions.YOURSELF:
+        const rawNumbers = await CLI.inputQuestion(
+          'Input numbers list (comma separated!)',
+        );
+        algorithmPayload = parseToIntArray(rawNumbers);
+        break;
+    }
+
     // run AlgorithmService.execute
+
+    // const algorithmService = new AlgorithmService();
+    // algorithmService.execute();
 
     console.log('Handle run algorithm');
   }
