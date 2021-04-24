2f05afeb1cb99d7c2c8ae811d05694680fa46c2b
Damian Szyszka <dszys@softserveinc.com>
2021-04-25 01:11:05 +0200

    Implement AlgorithmService

--- src/services/AlgorithmService/AlgorithmService.ts
+++ src/services/AlgorithmService/AlgorithmService.ts
@@ -1,3 +1,36 @@
 import { Algorithm } from '../../classes/Algorithm';
+import { AlgorithmResult } from '../../classes/Algorithm/types';
+import { logBreak, logSuccess, logError } from '../../utils/logger';
 
-export class AlgorithmService extends Algorithm {}
+export class AlgorithmService extends Algorithm {
+  private payload: unknown[];
+  private result: AlgorithmResult | null = null;
+
+  constructor(algorithm: Function, algorithmPayload: unknown[]) {
+    super(algorithm);
+    this.payload = algorithmPayload;
+  }
+
+  private validatePath(path: string) {}
+
+  private displayResult(): void {
+    logBreak();
+    console.log('Result');
+    logBreak();
+    logSuccess('Time: ' + this.result?.state.time);
+    logSuccess('Algorithm output: ' + this.result?.result);
+  }
+
+  public display(): void {
+    if (!this.result) {
+      logError('You have to execute algorithm first!');
+      return;
+    }
+    this.displayResult();
+  }
+
+  public execute(): void {
+    const result = this.run(this.payload);
+    this.result = result;
+  }
+}
