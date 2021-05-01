2e3deb2f7dcf331bcee5cb9908d77ba3e6daf2ba
Damian Szyszka <dszys@softserveinc.com>
2021-05-02 01:47:14 +0200

    Rewrite commander model to include available choices

--- src/models/commander.ts
+++ src/models/commander.ts
@@ -1,29 +1,60 @@
-export type TCommand = [string, string];
+export type TCommandOption = [string, string];
+export type TCommand = {
+  option: TCommandOption;
+  choices: string[] | null;
+};
 
 export const COMMAND_OPTIONS: TCommand[] = [
-  [
-    '-m, --mode  <mode>',
-    'Compare algorithms. Allowed values: ["create", "run", "compare", "utility"]',
-  ],
-  ['-o, --output_path <path>', 'Path to the output file'],
-  ['-n, --name <name>', 'Name of the file'],
-  [
-    '-b, --compare_by <compare_by>',
-    'Select by what you want to compare algorithms. Allowed options: ["performance"]',
-  ],
-  [
-    '-f --from <from>',
-    'Input source of algorithm payload file. Allowed options: ["file", "yourself", "generate"]',
-  ],
-  [
-    '-i, --input_path <path>',
-    'Type in path to the input file (algorithm etc.)',
-  ],
-  ['-q, --quantity <quantity>', 'Quantity of random numbers to generate'],
-  [
-    '-U, --utility_action <utility_action>',
-    'Select utility action you want to do. Allowed actions: ["generate_numbers"]',
-  ],
+  {
+    option: [
+      '-m, --mode  <mode>',
+      'Compare algorithms. Allowed values: ["create", "run", "compare", "utility"]',
+    ],
+    choices: ['create', 'run', 'compare', 'utility'],
+  },
+  {
+    option: ['-o, --output_path <path>', 'Path to the output file'],
+    choices: null,
+  },
+  {
+    option: ['-n, --name <name>', 'Name of the file'],
+    choices: null,
+  },
+  {
+    option: [
+      '-b, --compare_by <compare_by>',
+      'Select by what you want to compare algorithms. Allowed options: ["performance"]',
+    ],
+    choices: ['performance'],
+  },
+  {
+    option: [
+      '-f --from <from>',
+      'Input source of algorithm payload file. Allowed options: ["file", "yourself", "generate"]',
+    ],
+    choices: ['file', 'yourself', 'generate'],
+  },
+  {
+    option: [
+      '-i, --input_path <path>',
+      'Type in path to the input file (algorithm etc.)',
+    ],
+    choices: null,
+  },
+  {
+    option: [
+      '-q, --quantity <quantity>',
+      'Quantity of random numbers to generate',
+    ],
+    choices: null,
+  },
+  {
+    option: [
+      '-U, --utility_action <utility_action>',
+      'Select utility action you want to do. Allowed actions: ["generate_numbers"]',
+    ],
+    choices: ['generate_numbers'],
+  },
 ];
 
 export const COMMANDER_DESCRIPTION: string =
