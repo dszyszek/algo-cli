ba9d2520cd09dbd979801fa94f35a46681c24ec8
Damian Szyszka <dszys@softserveinc.com>
2021-05-01 21:54:04 +0200

    Implement Command class

--- src/classes/Command/Command.ts
+++ src/classes/Command/Command.ts
@@ -1,3 +1,28 @@
+import { Command as Commander } from 'commander';
 import { ICommand } from './types';
+import { TCommand } from '../../models/commander';
 
-export class Command implements ICommand {}
+export class Command implements ICommand {
+  private commanderInstance;
+  constructor() {
+    this.commanderInstance = new Commander();
+  }
+
+  public get currentOptions() {
+    return this.commanderInstance.opts();
+  }
+
+  public set options(options: TCommand[]) {
+    options.forEach((opt) => {
+      this.commanderInstance.option(...opt);
+    });
+  }
+
+  public set description(description: string) {
+    this.commanderInstance.description(description);
+  }
+
+  public parse(parseArgs?: string[]): void {
+    this.commanderInstance.parse(parseArgs);
+  }
+}
--- src/classes/Command/types.ts
+++ src/classes/Command/types.ts
@@ -1,1 +1,8 @@
-export interface ICommand {}
+import { TCommand } from '../../models/commander';
+
+export interface ICommand {
+  readonly currentOptions: any;
+  options: TCommand[];
+  description: string;
+  parse: (parseArgs?: string[]) => void;
+}
