0475896cf178f339f58b89d676a5af2e77defe19
Damian Szyszka <dszys@softserveinc.com>
2021-05-01 21:54:23 +0200

    Implement CommandService

--- src/services/CommandService/CommandService.ts
+++ src/services/CommandService/CommandService.ts
@@ -1,9 +1,40 @@
 import { Command } from '../../classes/Command/Command';
 import { ICommand } from '../../classes/Command/types';
+import {
+  COMMAND_OPTIONS,
+  COMMANDER_DESCRIPTION,
+  TCommand,
+} from '../../models/commander';
 
 export class CommandService {
   private commandInstance: ICommand;
-  constructor() {
+  private argSource: string[];
+
+  constructor(argSource: string[]) {
     this.commandInstance = new Command();
+    this.argSource = argSource;
+  }
+
+  public get options() {
+    return this.commandInstance.currentOptions;
+  }
+
+  private setOptions(options: TCommand[]): void {
+    this.commandInstance.options = options;
+  }
+
+  private setDescription(description: string): void {
+    this.commandInstance.description = description;
+  }
+
+  private prepare(): void {
+    this.setOptions(COMMAND_OPTIONS);
+    this.setDescription(COMMANDER_DESCRIPTION);
+  }
+
+  public getCommands() {
+    this.prepare();
+    this.commandInstance.parse(this.argSource);
+    return this.options;
   }
 }
