8cdbcf99c9dc7e2a2baa13df47eb945ad5070ab0
Damian Szyszka <dszys@softserveinc.com>
2021-05-01 15:18:54 +0200

    Move .getRandomNumbers to UtilityService

--- src/services/UtilityService/UtilityService.ts
+++ src/services/UtilityService/UtilityService.ts
@@ -1,8 +1,23 @@
 import { Utility } from '../../classes/Utility/Utility';
+import { logError, logInfo } from '../../utils/logger';
+import { CLI } from '../../classes/CLI';
 
 export class UtilityService {
-  public generateRandomNumbers(quantity: number): number[] {
-    const randomNumbers: number[] = Utility.generateRandomNumbers(quantity);
+  public async getRandomNumbers(): Promise<number[]> {
+    const quantityRaw: string = await CLI.inputQuestion(
+      'How many random numbers do you want?',
+    );
+    const qyantityParsed = parseInt(quantityRaw);
+
+    if (isNaN(qyantityParsed)) {
+      logError('You must input a number!');
+      CLI.exitFromProcess();
+    }
+    logInfo('Generating numbers...');
+    const randomNumbers: number[] = Utility.generateRandomNumbers(
+      qyantityParsed,
+    );
+
     return randomNumbers;
   }
 }
