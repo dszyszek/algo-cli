6129c64e4df7cddd02cc3b2959988803ec3c81e6
Damian Szyszka <dszys@softserveinc.com>
2021-05-03 01:30:56 +0200

    Enforce passing quantity to UtilityService.getRandomNumbers

--- src/services/UtilityService/UtilityService.ts
+++ src/services/UtilityService/UtilityService.ts
@@ -3,20 +3,13 @@ import { logError, logInfo } from '../../utils/logger';
 import { CLI } from '../../classes/CLI';
 
 export class UtilityService {
-  public async getRandomNumbers(): Promise<number[]> {
-    const quantityRaw: string = await CLI.inputQuestion(
-      'How many random numbers do you want?',
-    );
-    const qyantityParsed = parseInt(quantityRaw);
-
-    if (isNaN(qyantityParsed)) {
+  public async getRandomNumbers(quantity: number): Promise<number[]> {
+    if (isNaN(quantity)) {
       logError('You must input a number!');
       CLI.exitFromProcess();
     }
     logInfo('Generating numbers...');
-    const randomNumbers: number[] = Utility.generateRandomNumbers(
-      qyantityParsed,
-    );
+    const randomNumbers: number[] = Utility.generateRandomNumbers(quantity);
 
     return randomNumbers;
   }
